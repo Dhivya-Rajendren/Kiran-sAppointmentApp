8785a75145e9de1c017c74c537dab69434b4e04d
Dhivya-Rajendren <rajendren.dhivya@gmail.com>
2023-01-02 15:44:39 +0530

    Adding Dummy Data of patients

---
+++ Kiran'sAppointmentApp/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace Kiran_sAppointmentApp.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public long Contact { get; set; }
+
+        public string Email { get; set; }
+
+        public string PatientHistory { get; set; }
+    }
+}
