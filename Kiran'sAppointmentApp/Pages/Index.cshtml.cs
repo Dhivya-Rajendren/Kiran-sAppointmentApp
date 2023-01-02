b8dd0f15144fb8e50dca3c95f2d64477b57fb2b2
Dhivya-Rajendren <rajendren.dhivya@gmail.com>
2023-01-02 15:10:44 +0530

    Add project files.

---
+++ Kiran'sAppointmentApp/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kiran_sAppointmentApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<String> Doctors { get; set; }
+        public void OnGet()
+        {
+            string pageSubTitle = "Home Page - Appointment Booking";
+
+            //Data Dictionary 
+            ViewData["SubTitle"] = pageSubTitle;
+
+            Doctors = new List<string>();
+
+            Doctors.Add("Kumar");
+            Doctors.Add("Kiran");
+            Doctors.Add("Abdul");
+        }
+    }
+}
